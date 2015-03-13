8662aa8a54c1aab36514b78cd3dd8bdeadac60ba
Jason Johnston <jasonjoh@microsoft.com>
2015-03-13 16:49:56 -0400

    Simple contact list.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,22 @@
 require 'ruby_outlook'
-
+include ApplicationHelper
 class StaticPagesController < ApplicationController
   def home
     if logged_in?
+      outlook_client = RubyOutlook::Client.new
+      outlook_client.enable_fiddler = true
       
+      view_size = 30
+      page = 1
+      fields = [
+        "DisplayName"
+      ]
+      sort = { :sort_field => 'DisplayName', :sort_order => 'ASC' }
+      
+      wrapped_contacts = outlook_client.get_contacts session[:o365_access_token],
+        view_size, page, fields, sort
+        
+      @contacts = wrapped_contacts['value']
     end
   end
 end
