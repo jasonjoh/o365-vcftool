f5afc8dcdc96b52d3d586ff3206d73e7db78625a
Jason Johnston <jasonjoh@users.noreply.github.com>
2020-02-21 10:18:32 -0500

    Set vertical ruler only for ruby files
    Fixed a few random issues that rubocop linting via the VS code extension picked up

--- vcftool/test/application_system_test_case.rb
+++ vcftool/test/application_system_test_case.rb
@@ -2,6 +2,7 @@
 
 require 'test_helper'
 
+# ApplicationSystemTestCase
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
 end
--- vcftool/test/test_helper.rb
+++ vcftool/test/test_helper.rb
@@ -5,6 +5,7 @@ require_relative '../config/environment'
 require 'rails/test_help'
 
 module ActiveSupport
+  # TestCase
   class TestCase
     # Run tests in parallel with specified workers
     parallelize(workers: :number_of_processors, with: :threads)
