b737209fd5fa24b433a3f4be87a4a20de5fc5895
Jason Johnston <jasonjoh@users.noreply.github.com>
2020-02-21 09:56:04 -0500

    Final round of rubocop fixes
    Quashed all rubocop complaints about the default output from `rails new`.

--- vcftool/config/initializers/content_security_policy.rb
+++ vcftool/config/initializers/content_security_policy.rb
@@ -3,7 +3,7 @@
 
 # Define an application-wide content security policy
 # For further information see the following documentation
-# https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
+# https://developer.mozilla.org/docs/Web/HTTP/Headers/Content-Security-Policy
 
 # Rails.application.config.content_security_policy do |policy|
 #   policy.default_src :self, :https
@@ -13,19 +13,22 @@
 #   policy.script_src  :self, :https
 #   policy.style_src   :self, :https
 #   # If you are using webpack-dev-server then specify webpack-dev-server host
-#   policy.connect_src :self, :https, "http://localhost:3035", "ws://localhost:3035" if Rails.env.development?
+#   policy.connect_src :self, :https, "http://localhost:3035",
+#     "ws://localhost:3035" if Rails.env.development?
 
 #   # Specify URI for violation reports
 #   # policy.report_uri "/csp-violation-report-endpoint"
 # end
 
 # If you are using UJS then enable automatic nonce generation
-# Rails.application.config.content_security_policy_nonce_generator = -> request { SecureRandom.base64(16) }
+# Rails.application.config.content_security_policy_nonce_generator = ->
+#   request { SecureRandom.base64(16) }
 
 # Set the nonce only to specific directives
-# Rails.application.config.content_security_policy_nonce_directives = %w(script-src)
+# Rails.application.config.content_security_policy_nonce_directives =
+#   %w(script-src)
 
 # Report CSP violations to a specified URI
 # For further information see the following documentation:
-# https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy-Report-Only
+# https://developer.mozilla.org/docs/Web/HTTP/Headers/Content-Security-Policy-Report-Only
 # Rails.application.config.content_security_policy_report_only = true
