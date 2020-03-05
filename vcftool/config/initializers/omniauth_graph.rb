6a1a62cf52b520aa21162fa01fe3211b6117412e
Jason Johnston <jasonjoh@users.noreply.github.com>
2020-03-05 12:38:26 -0500

    More Travis tweaks

--- vcftool/config/initializers/omniauth_graph.rb
+++ vcftool/config/initializers/omniauth_graph.rb
@@ -6,10 +6,10 @@
 require 'microsoft_graph_auth'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  return if Rails.application.credentials.azure.nil?
-
-  provider :microsoft_graph_auth,
-           Rails.application.credentials.azure[:app_id],
-           Rails.application.credentials.azure[:app_secret],
-           scope: 'https://graph.microsoft.com/.default offline_access'
+  unless Rails.application.credentials.azure.nil?
+    provider :microsoft_graph_auth,
+             Rails.application.credentials.azure[:app_id],
+             Rails.application.credentials.azure[:app_secret],
+             scope: 'https://graph.microsoft.com/.default offline_access'
+  end
 end
